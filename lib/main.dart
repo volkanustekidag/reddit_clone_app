78ee12fe1d4abf5a760ecaf1bcc070039ca345fc
volkanustekidag <volkanustekidg@gmail.com>
2022-04-20 10:37:10 +0300

    home page screen is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reddit_clone_app/bloc/reddit_bloc.dart';
+import 'package:reddit_clone_app/bloc/reddit_bloc_observer.dart';
+import 'package:reddit_clone_app/repositories/reddit_repository.dart';
 import 'package:reddit_clone_app/screens/home_page.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  final RedditRepository redditRepository =
+      RedditRepository(httpClient: http.Client());
+
+  BlocOverrides.runZoned(
+      () => runApp(MyApp(
+            redditRepository: redditRepository,
+          )),
+      blocObserver: RedditBlocObserver());
 }
 
 class MyApp extends StatelessWidget {
+  final RedditRepository redditRepository;
+  MyApp({this.redditRepository});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomePage());
+    return BlocProvider(
+      create: (context) => RedditBloc(redditRepository: redditRepository),
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: HomePage()),
+    );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reddit_clone_app/bloc/reddit_bloc.dart';
+import 'package:reddit_clone_app/models/reddit_post.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -9,12 +12,59 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+
+    BlocProvider.of<RedditBloc>(context).add(RedditOnRequestEvent());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xffff6314),
         title: Text("RedditClone"),
       ),
-      body: Container(),
+      body: Container(
+        color: Colors.black12,
+        child: BlocBuilder<RedditBloc, RedditState>(
+          builder: (context, state) {
+            if (state is RedditLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is RedditSucces) {
+              RedditPost redditPost = state.redditPost;
+
+              return ListView.builder(
+                itemCount: redditPost.data.children.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                        title: Text(
+                          redditPost.data.children[index].data2.title,
+                        ),
+                        subtitle:
+                            redditPost.data.children[index].data2.thumbnail ==
+                                    "self"
+                                ? null
+                                : Image.network(redditPost
+                                    .data.children[index].data2.thumbnail)),
+                  );
+                },
+              );
+            }
+
+            if (state is RedditFailuer) {
+              return Center(
+                child: Text("have an error"),
+              );
+            }
+
+            return Container();
+          },
+        ),
+      ),
     );
   }
 }
